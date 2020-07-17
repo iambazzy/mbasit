4e4772926a275458422bab6813b45758be022295
bmir <bmir@sonicwall.com>
2020-07-17 19:51:42 +0530

    Updates

---
+++ precache-manifest.0ffa78b4d8f74ee52cabef294efa8caf.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617e9d7a9d9cbc6d28e1b2a60123d385",
+    "url": "/mbasit/index.html"
+  },
+  {
+    "revision": "27225a79c3d37130d8eb",
+    "url": "/mbasit/static/css/main.ae9c7fef.chunk.css"
+  },
+  {
+    "revision": "e38fcfb9523d14bf5457",
+    "url": "/mbasit/static/js/2.3478d945.chunk.js"
+  },
+  {
+    "revision": "27225a79c3d37130d8eb",
+    "url": "/mbasit/static/js/main.7614ef9c.chunk.js"
+  },
+  {
+    "revision": "2b38971b99c2a637d3e9",
+    "url": "/mbasit/static/js/runtime-main.80c439fe.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/mbasit/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/mbasit/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/mbasit/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/mbasit/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/mbasit/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/mbasit/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/mbasit/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/mbasit/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/mbasit/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/mbasit/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/mbasit/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/mbasit/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/mbasit/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/mbasit/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/mbasit/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/mbasit/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/mbasit/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/mbasit/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/mbasit/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/mbasit/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/mbasit/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/mbasit/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/mbasit/static/media/fa-solid-900.8e4a6dcc.eot"
+  }
+]);
